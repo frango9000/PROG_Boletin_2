7efab5b6959d21997599d33514b99312f9696d63
Francisco Sanchez <fsancheztemprano@danielcastelao.org>
2018-10-09 11:44:41 +0200

    boletin2_3

---
+++ Boletin2_3/Boletin2_3.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Boletin2_3;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author fsancheztemprano
+ */
+public class Boletin2_3 {
+    
+
+    public static void main(String[] args) {
+        
+        Scanner scanCantidad = new Scanner(System.in);
+        System.out.println("Introduce cantidad de billetes de 100€: ");
+        float cantidadBilletes100 = scanCantidad.nextFloat();
+        System.out.println("Introduce cantidad de billetes de 20€: ");
+        float cantidadBilletes20 = scanCantidad.nextFloat();
+        System.out.println("Introduce cantidad de billetes de 5€: ");
+        float cantidadBilletes5 = scanCantidad.nextFloat();
+        System.out.println("Introduce cantidad de monedas de 1€: ");
+        float cantidadMonedas1 = scanCantidad.nextFloat();
+        float totalDinero = ((cantidadBilletes100 * 100)+(cantidadBilletes20 * 20)+(cantidadBilletes5*5)+(cantidadMonedas1));
+        
+        System.out.println("Total : " + totalDinero + "€");
+    }        
+}
